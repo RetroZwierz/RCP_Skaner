2bd2d499c453540a0053e39bb8721b35c25177e0
pszaAtinea <psza@atinea.pl>
2022-05-26 13:32:16 +0200

    config

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ SCANER_ID = '2'
 ENTER = '2'
 LEAVE = '3'
 
-#API_IP = '10.1.9.161'
-API_IP = 'localhost'
+API_IP = '10.1.9.161'
+#API_IP = 'localhost'
 API_PORT = '5000'
 
-LOG_FILE = '../skaner.log'
+LOG_FILE = '/home/kuznia/skaner.log'
 #Przechowuje dane ze skanowania nie wysłane na api
-SCAN_BUFOR_FILE ='../buffer.txt'
+SCAN_BUFOR_FILE ='/home/kuznia/buffer.txt'
 #Przechowuje ostatni stan pracownika
-EMPLOYEE_LAST_STATUS ='../employeelaststatus.txt'
+EMPLOYEE_LAST_STATUS ='/home/kuznia/employeelaststatus.txt'
 
 #Co ile ma wysyłać informacje do api o aktywnośći
 HEART_BEAT_TIME = 1 #minutes
 #Co ile ma próbować wysłać dane ze skanowania które nie zostały wysłane na api przez zerwane połączenie
-SCAN_BUFFER_SEND_TIME = 5 #seconds
+SCAN_BUFFER_SEND_TIME = 60 #seconds

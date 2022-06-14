0afbe3e07f265d8ecca3bfc232844580330b863d
RetroZwierz <aleksander.aslamowicz@gmail.com>
2022-06-14 13:02:50 +0200

    Layout changed to 800x480 screen

--- config.py
+++ config.py
@@ -2,13 +2,12 @@ SECRET_KEY = 'vpU=Xm=zAQEUoUOpXhJF4oRhJmCaTBTQQDwvH40L1lXT'
 SALT = 'UWZUaldtWnE0dDd3IXolQw=='
 IV = 'blpyNHU3eCFBJUQqRy1LYQ=='
 
-SCANER_ID = '2'
+SCANER_ID = '100'
 
 ENTER = '2'
 LEAVE = '3'
 
-API_IP = '10.1.9.161'
-#API_IP = 'localhost'
+API_IP = '10.1.9.162'
 API_PORT = '5000'
 
 LOG_FILE = '/home/kuznia/skaner.log'
--- main.py
+++ main.py
@@ -9,15 +9,27 @@ from scanner import Scanner
 from scheduler import start_scheduled_jobs
 
 
-customtkinter.set_appearance_mode("Dark")
+customtkinter.set_appearance_mode("Ligh")
 customtkinter.set_default_color_theme("blue")
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 
 class App(customtkinter.CTk):
-    INITIAL_LABEL_COLOR = ("gray70", "gray25")
+    INITIAL_LABEL_COLOR = ("gray25")
+    INITIAL_WIDTH = 800
+    INITIAL_HEIGHT = 50
+    INITIAL_FONT = ("Arial", 22, "bold")
+    TIME_FONT = ("Arial", 32, "bold")
+    RESPONSE_WIDTH = 800
+    RESPONSE_HEIGHT = 480
+    RESPONSE_FONT = ("Arial", 38, "bold")
 
     APP_NAME = "GUI"
+
+    def present_time(self):
+        display_time = time.strftime("%H:%M:%S")
+        self.label_3.config(text=display_time)
+        self.label_3.after(1000, self.present_time)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -47,10 +59,18 @@ class App(customtkinter.CTk):
 
         self.label_text = tkinter.StringVar()
         self.label_text.set("Zeskanuj kod QR")
-        self.label_1 = customtkinter.CTkLabel(width=200, height=100,
+        self.label_1 = customtkinter.CTkLabel(width=App.INITIAL_WIDTH, height=App.INITIAL_HEIGHT,
                                               fg_color=App.INITIAL_LABEL_COLOR, textvariable=self.label_text)
-        self.label_1.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-        self.label_1.configure(text_color='#bec1c2')
+        self.label_1.place(relx=0.5, rely=1.0, anchor=tkinter.S)
+        self.label_1.config(text_color='#bec1c2')
+        self.label_1.config(font=App.INITIAL_FONT)
+
+        self.label_3 = customtkinter.CTkLabel(width=200, height=50, fg_color='white')
+        self.label_3.place(relx=1.0, rely=0.0, anchor=tkinter.NE)
+        self.label_3.config(text_color='black')
+        self.label_3.config(font=App.TIME_FONT)
+
+        self.present_time()
 
         self.entry_1.focus()
 
@@ -62,26 +82,30 @@ class App(customtkinter.CTk):
                 self.code.set('')
             return
         success, message, employee_id = self.scanner.scan(code)
+        self.label_text_2 = tkinter.StringVar()
+        self.label_text_2.set("")
+        self.label_2 = customtkinter.CTkLabel(width=App.INITIAL_WIDTH, height=App.INITIAL_HEIGHT,
+                                              fg_color=App.INITIAL_LABEL_COLOR, textvariable=self.label_text_2)
+        self.label_2.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.label_2.config(width=App.RESPONSE_WIDTH, height=App.RESPONSE_HEIGHT)
+        self.label_2.config(font=App.RESPONSE_FONT)
+        self.label_2.config(text_color='white')
         if success:
-            self.label_text.set(message + "\n ID: " + employee_id)
+            self.label_text_2.set(message + "\n ID: " + employee_id)
             if message == "Zarejestrowano czas wejścia.":
-                self.label_1.configure(fg_color='green')
-                self.label_1.update()
+                self.label_2.config(fg_color='green')
+                self.label_2.update()
             if message == "Zarejestrowano czas wyjścia.":
-                self.label_1.configure(fg_color='yellow')
-                self.label_1.configure(text_color='black')
-                self.label_1.update()
+                self.label_2.config(fg_color='yellow')
+                self.label_2.config(text_color='black')
+                self.label_2.update()
         else:
-            self.label_text.set(message)
-            self.label_1.configure(fg_color='red')
-            self.label_1.update()
+            self.label_text_2.set(message)
+            self.label_2.config(fg_color='red')
+            self.label_2.update()
 
-        time.sleep(5)
+        self.label_2.after(5000, self.label_2.destroy)
         self.code.set('')
-        self.label_text.set('Zeskanuj kod QR')
-        self.label_1.configure(fg_color=App.INITIAL_LABEL_COLOR)
-        self.label_1.configure(text_color='#bec1c2')
-
 
     def on_closing(self, event=0):
         self.destroy()

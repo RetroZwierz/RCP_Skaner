03daaac2bd730dac8ecdd135ddfe6bd48e921644
RetroZwierz <aleksander.aslamowicz@gmail.com>
2022-06-28 17:15:12 +0200

    Updated config file

--- config.py
+++ config.py
@@ -2,20 +2,19 @@ SECRET_KEY = 'vpU=Xm=zAQEUoUOpXhJF4oRhJmCaTBTQQDwvH40L1lXT'
 SALT = 'UWZUaldtWnE0dDd3IXolQw=='
 IV = 'blpyNHU3eCFBJUQqRy1LYQ=='
 
-SCANER_ID = '2'
+SCANER_ID = '100'
 
 ENTER = '2'
 LEAVE = '3'
 
-API_IP = '10.1.9.161'
-#API_IP = 'localhost'
+API_IP = '10.1.9.162'
 API_PORT = '5000'
 
-LOG_FILE = '/home/kuznia/skaner.log'
+LOG_FILE = '/var/log/RCP/skaner.log'
 #Przechowuje dane ze skanowania nie wysłane na api
-SCAN_BUFOR_FILE ='/home/kuznia/buffer.txt'
+SCAN_BUFOR_FILE ='/var/log/RCP/buffer.log'
 #Przechowuje ostatni stan pracownika
-EMPLOYEE_LAST_STATUS ='/home/kuznia/employeelaststatus.txt'
+EMPLOYEE_LAST_STATUS ='/var/log/RCP/employeelaststatus.log'
 
 #Co ile ma wysyłać informacje do api o aktywnośći
 HEART_BEAT_TIME = 15 #minutes

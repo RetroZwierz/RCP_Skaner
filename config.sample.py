2e00e7a1a82b5aa57afd0748108a35e40fa17412
RetroZwierz <aleksander.aslamowicz@gmail.com>
2022-06-28 17:26:48 +0200

    SQL changed to use card_id instead of employee_id

---
+++ config.sample.py
@@ -0,0 +1,22 @@
+SECRET_KEY = 'vpU=Xm=zAQEUoUOpXhJF4oRhJmCaTBTQQDwvH40L1lXT'
+SALT = 'UWZUaldtWnE0dDd3IXolQw=='
+IV = 'blpyNHU3eCFBJUQqRy1LYQ=='
+
+SCANER_ID = '2'
+
+ENTER = '2'
+LEAVE = '3'
+
+API_IP = '10.1.9.161'
+API_PORT = '5000'
+
+LOG_FILE = '/home/kuznia/skaner.log'
+#Przechowuje dane ze skanowania nie wysłane na api
+SCAN_BUFOR_FILE ='/home/kuznia/buffer.txt'
+#Przechowuje ostatni stan pracownika
+EMPLOYEE_LAST_STATUS ='/home/kuznia/employeelaststatus.txt'
+
+#Co ile ma wysyłać informacje do api o aktywnośći
+HEART_BEAT_TIME = 15 #minutes
+#Co ile ma próbować wysłać dane ze skanowania które nie zostały wysłane na api przez zerwane połączenie
+SCAN_BUFFER_SEND_TIME = 60 #seconds
--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class App(customtkinter.CTk):
         self.entry_1 = tkinter.Text()
         self.entry_1.place(relx=0.5, rely=0.5)
         self.entry_1.bind('<Return>',lambda event: self.input_callback())
-
+        
         self.image_label = tkinter.Label(master=self, image=self.bg_image)
         self.image_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
@@ -78,7 +78,7 @@ class App(customtkinter.CTk):
         if not '\n' in code:
             self.entry_1.delete("1.0","end")
             return
-
+            
         success, message, employee_id = self.scanner.scan(code)
         self.label_text_2 = tkinter.StringVar()
         self.label_text_2.set("")
@@ -97,6 +97,21 @@ class App(customtkinter.CTk):
                 self.label_2.config(fg_color='yellow')
                 self.label_2.config(text_color='black')
                 self.label_2.update()
+        elif "Podwójny skan " in message:
+                if "Wejście" in message:
+                    self.label_text_2.set(message + "\n ID: " + employee_id)
+                    self.label_2.config(fg_color='green')
+                    self.label_2.update()
+                elif "Wyjście" in message:
+                    self.label_text_2.set(message + "\n ID: " + employee_id)
+                    self.label_2.config(fg_color='yellow')
+                    self.label_2.config(text_color='black')
+                    self.label_2.update()
+                else:
+                    self.label_text_2.set(message + "\n ID: " + employee_id)
+                    self.label_2.config(fg_color='gray')
+                    self.label_2.config(text_color='black')
+                    self.label_2.update()
         else:
             self.label_text_2.set(message)
             self.label_2.config(fg_color='red')
